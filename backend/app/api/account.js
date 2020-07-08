ca7d1ae1b641646b90591474c4e154414312a930
Dave Bennett <dnbennett@hotmail.co.uk>
2020-07-08 21:50:32 +0100

    dulpicate users prevented

--- backend/app/api/account.js
+++ backend/app/api/account.js
@@ -1,12 +1,24 @@
 const { Router } = require('express');
 const AccountTable = require('../account/table.js');
+const { hash } = require('../account/helper');
 
 const router = new Router();
 
 router.post('/signup', (req, res, next) => {
     const { username, password } = req.body;
+    const usernameHash = hash(username);
+    const passwordHash = hash(password);
 
-    AccountTable.storeAccount({ username, password })
+    AccountTable.getAccount({ usernameHash })
+        .then(({ account }) => {
+            if (!account) {
+                return AccountTable.storeAccount({ usernameHash, passwordHash })
+            } else {
+                const error = new Error('This username has already been taken');
+                error.statusCode = 409;
+                throw error;
+            }
+        })
         .then(() => res.json({ message: 'success!!' }))
         .catch(error => next(error));
 });
